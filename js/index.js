2e02ea7734d5e630be91320b0f9d36e6c86d1b97
刘豪亮 <271072547@qq.com>
2018-06-09 22:07:18 +0800

    临时提交标注点

--- js/index.js
+++ js/index.js
@@ -45,16 +45,41 @@ function draw(points) {
     canvas.width = img.width;
     canvas.height = img.height;
     var ctx = canvas.getContext('2d');
-    ctx.beginPath();
+    // ctx.beginPath();
     ctx.drawImage(img,0,0,img.width,img.height);
-    ctx.fillStyle = '#FF0000';
+    ctx.fillStyle = 'cyan';
+    ctx.strokeStyle = 'red';
+    ctx.lineJoin = 'round';
+    ctx.lineWidth = 2;
+    var radius = 3;
+    // ctx.save();
     if (points.length>0){
-        ctx.moveTo(points[0].x * canvas.width, points[0].y * canvas.height); //起始位置  
+        var arcX = points[0].x * canvas.width;
+        var arcY = points[0].y * canvas.height;
+        ctx.strokeStyle = 'cyan';
+        ctx.arc(arcX, arcY, radius, 0, 2*Math.PI, false);
+        ctx.fill();
+
         points.forEach((point,idx) => {
             if (idx!=0){
-                ctx.lineTo(point.x * canvas.width, point.y * canvas.height); //停止位置  
+                // ctx.beginPath();
+                var nextArcX = point.x * canvas.width;
+                var nextArcY = point.y * canvas.height;
+                var lastArcX = points[idx-1].x*canvas.width;
+                var lastArcY = points[idx-1].y*canvas.height;
+                ctx.restore();
+                ctx.moveTo(lastArcX,lastArcY)
+
+                ctx.lineTo(nextArcX, nextArcY); //停止位置  
+                
+                //再描一次上次的点
+                ctx.beginPath();
+                ctx.arc(lastArcX, lastArcY, radius, 0, 2*Math.PI, false);
+                ctx.arc(nextArcX, nextArcY, radius, 0, 2*Math.PI, false);
+
+                ctx.fill();
+                ctx.stroke();
             }
         });
-        ctx.stroke();
     }
 }
