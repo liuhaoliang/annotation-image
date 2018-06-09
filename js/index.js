d97429fa82560c7d8fc6f77e1aa2da8c97cf0bac
刘豪亮 <271072547@qq.com>
2018-06-09 22:48:15 +0800

    设置点距的误差范围

--- js/index.js
+++ js/index.js
@@ -3,6 +3,13 @@ var content = document.getElementsByClassName('content')[0];
 var points = [];
 var canvas = document.getElementById('myCanvas');
 var undo = document.getElementById('undo');
+var clear = document.getElementById('clear');
+//设置接头半径
+var radius = 4;
+//设置线宽
+var lineWidth = 2;
+//设置误差范围
+var eviation = 0.001;
 
 canvas.onclick = function (e) {
     var offsetX = e.offsetX;
@@ -12,6 +19,17 @@ canvas.onclick = function (e) {
     var height = image.height;
     var scaleX = offsetX / width;
     var scaleY = offsetY / height;
+
+
+    points.forEach((point, idx) => {
+        var s = Math.pow((point.x-scaleX),2)+Math.pow((point.y-scaleY),2);
+        console.log('+++++'+s)
+        if (s<0.001) {
+            scaleX = point.x;
+            scaleY = point.y;
+        }
+    });
+
     points.push({
         x: scaleX,
         y: scaleY
@@ -19,6 +37,7 @@ canvas.onclick = function (e) {
     draw(points);
 };
 
+//撤销操作
 undo.onclick = function (e) {
     if (points.length > 0) {
         points.pop();
@@ -26,8 +45,11 @@ undo.onclick = function (e) {
     }
 };
 
-
-
+//删除操作
+clear.onclick = function (e) {
+    points.splice(0,points.length);
+    draw(points);
+};
 
 if (img.height == 0) {
     targetImage.load(function (s) {
@@ -47,8 +69,7 @@ function draw(points) {
     ctx.fillStyle = 'cyan';
     ctx.strokeStyle = 'red';
     ctx.lineJoin = 'round';
-    ctx.lineWidth = 2;
-    var radius = 4;
+    ctx.lineWidth = lineWidth;
     if (points.length > 0) {
         var arcX = points[0].x * canvas.width;
         var arcY = points[0].y * canvas.height;
